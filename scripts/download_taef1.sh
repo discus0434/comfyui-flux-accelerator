c9d422c45144135279cb26bdef1c42882a9fcd0a
discus0434 <discus0434@gmail.com>
2024-09-22 18:37:06 +0900

    fix download_taef1.sh

--- scripts/download_taef1.sh
+++ scripts/download_taef1.sh
@@ -1,4 +1,4 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VAE_DIR="${CURRENT_DIR}/../../models/vae_approx"
+VAE_DIR="${CURRENT_DIR}/../../../models/vae_approx"
 wget https://github.com/madebyollin/taesd/raw/refs/heads/main/taef1_encoder.pth -P "${VAE_DIR}"
 wget https://github.com/madebyollin/taesd/raw/refs/heads/main/taef1_decoder.pth -P "${VAE_DIR}"
